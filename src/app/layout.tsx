84fe483403259ddc08d21fc1b59f86408fa0827a
MAHABUB-ALAM <mahabubalom72091@gmail.com>
2025-11-10 11:11:40 +0300

    added some code with error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,40 +1,23 @@
-import LoginSuccessToast from "@/components/shared/LoginSuccessToast";
-import LogoutSuccessToast from "@/components/shared/LogoutSuccessToast";
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import { Toaster } from "sonner";
-import "./globals.css";
+import DashboardNavbar from "@/components/modules/Dashboard/DashboardNavbar";
+import DashboardSidebar from "@/components/modules/Dashboard/DashboardSidebar";
+import React from "react";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export const metadata: Metadata = {
-  title: "PH-Health-Care",
-  description: "A healthcare application built with Next.js",
+const CommonDashboardLayout = async ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  return (
+    <div className="flex h-screen overflow-hidden">
+      <DashboardSidebar />
+      <div className="flex flex-1 flex-col overflow-hidden">
+        <DashboardNavbar />
+        <main className="flex-1 overflow-y-auto bg-muted/10 p-4 md:p-6">
+          <div className="max-w-7xl">{children}</div>
+        </main>
+      </div>
+    </div>
+  );
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-        <Toaster position="top-right" richColors />
-        <LoginSuccessToast />
-        <LogoutSuccessToast />
-      </body>
-    </html>
-  );
-}
+export default CommonDashboardLayout;
