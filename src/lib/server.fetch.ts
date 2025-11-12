6faece1186033700cd7bd38b2058809b6615c8f9
MAHABUB-ALAM <mahabubalom72091@gmail.com>
2025-11-12 20:54:56 +0300

    added the some commonets

---
+++ src/lib/server.fetch.ts
@@ -0,0 +1,45 @@
+import { getCookie } from "@/services/tokenHandlers";
+
+const BACKEDN_API_URL =
+  process.env.NEXT_PUBLIC_API_URL || "http://localhost:9000/api/v1";
+
+const serverFetchHelper = async (
+  endpoint: string,
+  options: RequestInit
+): Promise<Response> => {
+  const { headers, ...restOptions } = options;
+
+  const accessToken = await getCookie("accessToken");
+
+  const response = await fetch(`${BACKEDN_API_URL}${endpoint}`, {
+    headers: {
+      ...headers,
+      //   ...(accessToken ? { Authorization: `Bearer ${accessToken}` } : {}),
+      Cookie: accessToken ? `accessToken=${accessToken}` : "",
+    },
+    ...restOptions,
+  });
+  return response;
+};
+
+export const serverFetch = {
+  get: async (endpoint: string, options: RequestInit = {}): Promise<Response> =>
+    serverFetchHelper(endpoint, { ...options, method: "GET" }),
+  post: async (
+    endpoint: string,
+    options: RequestInit = {}
+  ): Promise<Response> =>
+    serverFetchHelper(endpoint, { ...options, method: "POST" }),
+  put: async (endpoint: string, options: RequestInit = {}): Promise<Response> =>
+    serverFetchHelper(endpoint, { ...options, method: "PUT" }),
+  patch: async (
+    endpoint: string,
+    options: RequestInit = {}
+  ): Promise<Response> =>
+    serverFetchHelper(endpoint, { ...options, method: "PATCH" }),
+  delete: async (
+    endpoint: string,
+    options: RequestInit = {}
+  ): Promise<Response> =>
+    serverFetchHelper(endpoint, { ...options, method: "DELETE" }),
+};
